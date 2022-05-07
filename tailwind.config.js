bd3cbd968fe6811fb3ebe157cf1beeae542ccc5b
Christian Dueck <christian@dueck.com.br>
2022-05-07 14:27:02 -0300

    Created Widget Button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ['brand']: '#8257E5',
+        ['brand-hover']: '#996DFF',
+        ['text-on-brand-color']: '#FFFFFF',
+      }
+    },
   },
   plugins: [],
 }
